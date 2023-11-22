6536b50bea3174fae52d93ca4245c9ba599bbdd8
tylerdionne <143131384+tylerdionne@users.noreply.github.com>
2023-11-22 12:12:29 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-def main():
-    app = QApplication([])
-    xml_parser = XMLParser("florida_drivers_test.xml")
-    questions = xml_parser.parse()
-    ui = DMVDriverTestUI(questions)
-    ui.show()
-    app.exec_()
-
-    main()
