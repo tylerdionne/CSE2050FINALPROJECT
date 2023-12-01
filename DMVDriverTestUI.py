d11b12f56ef272016ed4dde194e4728c6a646877
Christian Ott II <cott2020@my.fit.edu>
2023-12-01 10:05:15 -0500

    Updated UI

--- DMVDriverTestUI.py
+++ DMVDriverTestUI.py
@@ -80,6 +80,4 @@ class DMVDriverTestUI(QWidget):
             for i in reversed(range(layout.count())):
                 widget_to_remove = layout.itemAt(i).widget()
                 layout.removeWidget(widget_to_remove)
-
-
                 widget_to_remove.setParent(None)
